de0ec1a48a7cf5869763cb41c318501c5c70230c
Ramazan <rablyatipov@bk.ru>
2018-10-01 18:53:35 +0300

    Labs

--- Rocket-launch_start-skeleton/app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchAndSpinAnimatorSetAnimatorActivity.java
+++ Rocket-launch_start-skeleton/app/src/main/java/com/raywenderlich/rocketlaunch/animationactivities/LaunchAndSpinAnimatorSetAnimatorActivity.java
@@ -1,8 +1,32 @@
 package com.raywenderlich.rocketlaunch.animationactivities;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+
 public class LaunchAndSpinAnimatorSetAnimatorActivity extends BaseAnimationActivity {
   @Override
   protected void onStartAnimation() {
+    //1
+    ValueAnimator positionAnimator = ValueAnimator.ofFloat(0, -mScreenHeight);
+    //2
+    positionAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float value = (float) animation.getAnimatedValue();
+        mRocket.setTranslationY(value);
+      }
+    });
 
+    //3
+    ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(mRocket, "rotation", 0, 180f);
+    //4
+    AnimatorSet animatorSet = new AnimatorSet();animatorSet.play(positionAnimator).with(rotationAnimator);
+    //5
+    animatorSet.play(positionAnimator).with(rotationAnimator);
+    //6
+    animatorSet.setDuration(DEFAULT_ANIMATION_DURATION);
+    animatorSet.start();
   }
 }
